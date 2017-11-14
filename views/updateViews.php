1587180284f9bae0c980998dfb62414be2ee1d73
sanouo <sanouo591@gmail.com>
2017-11-14 10:42:52 +0100

    insertion file sql

--- views/updateViews.php
+++ views/updateViews.php
@@ -27,6 +27,7 @@
     <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
       <button class="dropdown-item" type="button">Action</button>
       <button class="dropdown-item" type="button">Another action</button>
+      <button class="dropdown-item" type="button">Another action</button>
       <button class="dropdown-item" type="button">Something else here</button>
     </div>
   </div>
